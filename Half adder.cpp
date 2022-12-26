4b0396a6fd7778191b8b73d468cfdbcf8e4593f8
DivyanshChoudhary391 <114098621+DivyanshChoudhary391@users.noreply.github.com>
2022-12-26 15:17:20 +0530

    Create Half adder.cpp

---
+++ Half adder.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void Half_Adder(int A , int B)
+{
+    int sum;
+    int carry;
+
+    sum = A^B;
+
+    carry = A & B;
+
+    cout<<"Sum is :"<<sum<<endl;
+    cout<<"Carry is :"<<carry<<endl;
+}
+
+int main()
+{
+    int A,B;
+    cin>>A>>B;
+    Half_Adder(A,B);
+    return 0;
+
+}
